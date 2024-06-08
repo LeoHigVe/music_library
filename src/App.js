3422be589a9ee4c1c2de32549412ad7f87b5eb5d
leo-mp3 <leonardohigareda@Diet-Mountain-Dew.local>
2024-06-08 13:44:37 -0700

    added galleryitem

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect,useState } from "react";
 import SearchBar from "./components/SearchBar";
-import Gallery from "./components/gallery";
+import Gallery from "./components/Gallery";
 function App() {
   let [search, setSearch] = useState('')
   let [message, setMessage] = useState('Search for music !')
